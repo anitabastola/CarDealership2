3993e217e219067d98aa98947df8d48a483e0ca6
student <abastola@pgh.yearup.org>
2024-05-17 12:04:04 -0400

    added constructor and getters&setters for contract

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
-public class LeaseContract {
+public class LeaseContract extends Contract {
+    private double expectedEndingValue;
+    private double leaseFee;
+
+    public LeaseContract(String date, String customerName, String customerEmail, double vehicleSold, double expectedEndingValue, double leaseFee) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.expectedEndingValue = expectedEndingValue;
+        this.leaseFee = leaseFee;
+    }
+
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public void setExpectedEndingValue(double expectedEndingValue) {
+        this.expectedEndingValue = expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    public void setLeaseFee(double leaseFee) {
+        this.leaseFee = leaseFee;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return 0;
+    }
 }
--- src/main/java/com/pluralsight/SalesContract.java
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -1,4 +1,62 @@
 package com.pluralsight;
 
-public class SalesContract {
+public class SalesContract extends Contract {
+    private double salesTaxAmount;
+    private double recordingFees;
+    private double processingFees;
+    private boolean finance;
+
+    public SalesContract(String date, String customerName, String customerEmail, double vehicleSold, double salesTaxAmount, double recordingFees, double processingFees, boolean finance) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.salesTaxAmount = salesTaxAmount;
+        this.recordingFees = recordingFees;
+        this.processingFees = processingFees;
+        this.finance = finance;
+
+    }
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+    public void setSalesTaxAmount(double salesTaxAmount) {
+        this.salesTaxAmount = salesTaxAmount;
+    }
+
+    public double getRecordingFees() {
+        return recordingFees;
+    }
+
+    public void setRecordingFees(double recordingFees) {
+        this.recordingFees = recordingFees;
+    }
+
+    public double getProcessingFees() {
+        return processingFees;
+    }
+
+    public void setProcessingFees(double processingFees) {
+        this.processingFees = processingFees;
+    }
+
+    public boolean isFinance() {
+        return finance;
+    }
+
+    public void setFinance(boolean finance) {
+        this.finance = finance;
+    }
+
+    @Override
+    public double getTotalPrice() {
+
+        return 0;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        return 0;
+    }
+
+
 }
